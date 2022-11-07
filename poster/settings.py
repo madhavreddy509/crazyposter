d3d016b0d5d922be15f86774fb1421ce27e80f17
madhavreddy509 <madhavreddy00570@gmail.com>
2022-11-07 22:11:47 +0530

    updated settings

--- poster/settings.py
+++ poster/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hw!v@h&59$)a)v)c57yg5+1(w0%=(+)02#)ct-f&-)-_1efx44'
-#SECRET_KEY=os.environ.get('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-hw!v@h&59$)a)v)c57yg5+1(w0%=(+)02#)ct-f&-)-_1efx44'
+SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
  
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tinyposterapp.herokuapp.com']
 
 
 # Application definition
