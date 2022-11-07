229f8ac96f46d1d1dd568373c0dabd4c4a0fe33c
madhavreddy509 <madhavreddy00570@gmail.com>
2022-11-07 22:26:46 +0530

    changed req

--- poster/settings.py
+++ poster/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-#import django_heroku
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -153,4 +153,4 @@ EMAIL_USE_TLS=True
 EMAIL_HOST_USER=os.environ.get('e_mail')
 EMAIL_HOST_PASSWORD=os.environ.get('app_password')
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
